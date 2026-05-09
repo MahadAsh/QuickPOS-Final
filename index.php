5be20ec6a1df75022dd9ad0285e251dfe205dcde
MahadAsh <mahad8ash@gmail.com>
2026-05-09 18:30:12 +0500

    [POS-22] Add social media icon links

--- index.php
+++ index.php
@@ -79,5 +79,14 @@
             <button type="submit" class="btn" style="margin-top: 10px;">Send Message</button>
         </form>
     </section>
+
+    <footer class="glass">
+        <div class="social-icons">
+            <a href="#"><i class="fab fa-facebook-f"></i></a>
+            <a href="#"><i class="fab fa-twitter"></i></a>
+            <a href="#"><i class="fab fa-instagram"></i></a>
+            <a href="#"><i class="fab fa-linkedin-in"></i></a>
+        </div>
+    </footer>
 </body>
 </html>
